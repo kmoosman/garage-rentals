91ee22f2f07965c6b70c00453e2c30b917f225db
Katie Moosman <moosman.katie@gmail.com>
2019-07-01 17:04:23 -0600

    added filter to filter venues to state. Also refactored to one constructor for Venue

--- app/assets/javascripts/venue.js
+++ app/assets/javascripts/venue.js
@@ -1,40 +1,11 @@
-console.log('Hello from venue.js')
 $(document).ready(function() {
     //Object constructor w/prototype function
-    function Venue(name, city, state, id, events) {
-      this.name = name;
-      this.city = city;
-      this.state = state;
-      this.id = id
-      this.events = events
-      this.header = function() {
-          
-        return `<h4>${name}</h4>
-                <p>${city}, ${state}</p>`
-      }
-      this.generateCards = function() {
-        events.forEach(event => {
-            console.log(event)
-            var html = `<card class="card" style="display: inline-block; text-align: center; width: 300px; height: 150px; margin: 20px; padding-top: 20px ">
-                            <h4 style="color: #4C6AC1">${event.event_name}</h4>
-                            <p>${event.date}</p>
-                            <a href="http://localhost:3000/venues/${id}"class="btn btn-primary" style="margin: -20px">Details</a>
-                        </card>`
-            $("#destinations").append(html)
-                    
-          });}
-      }
-
-  
     $.get(`${window.location.href}.json`, function(data, status){
         var venue = new Venue(data.name, data.city, data.state, data.id, data.events)
         console.log(data)
 
-        $(".venue_details").append(venue.header);
-        venue.generateCards()
+        $(".venue_details").append(venue.eventHeader);
+        venue.genEventCards()
       })
-    
- 
-
-      
+  
   })
--- app/assets/javascripts/venues.js
+++ app/assets/javascripts/venues.js
@@ -1,5 +1,3 @@
-console.log('Hello from venues.js')
-
 $(document).ready(function() {
   //Object constructor w/prototype function
   // $(function () {
@@ -12,7 +10,7 @@ $(document).ready(function() {
  
       posting.done(function(data) {
         var venue = new Venue(data.name, data.city, data.state, data.id)
-        $(".venues_list").append(venue.html);
+        $(".venues_list").append(venue.venueHtml);
   
       });
     });
@@ -21,41 +19,27 @@ $(document).ready(function() {
     console.log("I've already loaded venues to the dom")
    
   } else {
-      function Venue(name, city, state, id) {
-        this.name = name;
-        this.city = city;
-        this.state = state;
-        this.id = id
-        this.html = function() {
-          return `<card class="card" style="display: inline-block;width: 300px; height: 150px; margin: 20px; padding-top: 20px ">
-                    <h4>${name}</h4>
-                    <p>${city}, ${state}</p>
-                    <a href="http://localhost:3000/venues/${id}"class="btn btn-primary" style="margin: -20px">Details</a>
-                  </card>`
-        }
-      }
-     
-      $.get(`/venues.json`, function(data, status){
-        data.forEach(element => {
-          var venue = new Venue(element.name, element.city, element.state, element.id)
-          $(".venues_list").append(venue.html);
-        });
-      
-    
-        })
+      fetchVenues()
     
         $('#texas').click(function(event) {
           //prevent form from submitting the default way
           event.preventDefault();
-          console.log("you have clicked on Texas")
-          // var values = $(this).serialize();
-          // var posting = $.post('/venues', values);
-     
-          // posting.done(function(data) {
-          //   var venue = new Venue(data.name, data.city, data.state, data.id)
-          //   $(".venues_list").append(venue.html);
-      
-          // });
+          $(".venues_list").empty()
+          fetchVenues("Texas")
+        });
+
+        $('#utah').click(function(event) {
+          //prevent form from submitting the default way
+          event.preventDefault();
+          $(".venues_list").empty()
+          fetchVenues("Utah")
+        });
+
+        $('#cali').click(function(event) {
+          //prevent form from submitting the default way
+          event.preventDefault();
+          $(".venues_list").empty()
+          fetchVenues("California")
         });
 
 
@@ -63,3 +47,55 @@ $(document).ready(function() {
 
     
 })
+
+function fetchVenues(filter) {
+    $.get(`/venues.json`, function(data, status){
+      if (filter) {
+        data.forEach(element => {
+          if (element.state == filter) {
+            var venue = new Venue(element.name, element.city, element.state, element.id)
+            $(".venues_list").append(venue.venueHtml);
+          }
+          
+        });
+      } else {
+        data.forEach(element => {
+          var venue = new Venue(element.name, element.city, element.state, element.id)
+          $(".venues_list").append(venue.venueHtml);
+        })
+      }
+  })  
+}
+
+//constructo for venue
+function Venue(name, city, state, id, events) {
+  this.name = name;
+  this.city = city;
+  this.state = state;
+  this.id = id
+  this.events = events
+  this.venueHtml = function() {
+    return `<card class="card" style="display: inline-block;width: 300px; height: 150px; margin: 20px; padding-top: 20px ">
+              <h4>${name}</h4>
+              <p>${city}, ${state}</p>
+              <a href="http://localhost:3000/venues/${id}"class="btn btn-primary" style="margin: -20px">Details</a>
+            </card>`
+  }
+  
+  this.eventHeader = function() {
+        return `<h4>${name}</h4>
+                <p>${city}, ${state}</p>`
+      }
+  this.genEventCards = function() {
+        events.forEach(event => {
+            console.log(event)
+            var html = `<card class="card" style="display: inline-block; text-align: center; width: 300px; height: 150px; margin: 20px; padding-top: 20px ">
+                            <h4 style="color: #4C6AC1">${event.event_name}</h4>
+                            <p>${event.date}</p>
+                            <a href="http://localhost:3000/venues/${id}"class="btn btn-primary" style="margin: -20px">Details</a>
+                        </card>`
+            $("#destinations").append(html)
+                    
+          });}
+      
+}
